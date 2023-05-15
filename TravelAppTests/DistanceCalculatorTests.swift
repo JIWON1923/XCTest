427ace56d0ae01852b467523dba50e9b552138c5
JIWON1923 <zest1923@gmail.com>
2023-05-15 23:07:52 +0900

    [Feat] 도시 사이 거리 테스트 작성

--- TravelAppTests/DistanceCalculatorTests.swift
+++ TravelAppTests/DistanceCalculatorTests.swift
@@ -10,25 +10,23 @@ import XCTest
 
 final class DistanceCalculatorTests: XCTestCase {
     
-    override func setUpWithError() throws {
+    var calculator: DistancaCaculator!
+    
+    override func setUp() {
+        calculator = DistancaCaculator()
+        super.setUp()
     }
-
-    override func tearDownWithError() throws {
-    }
-
-    func testExample() throws {
-    }
-
-    func testPerformanceExample() throws {
-        self.measure {
-        }
+    
+    override func tearDown() {
+        calculator = nil
+        super.tearDown()
     }
     
     // 1. city function이 제대로 동작하는지 확인하기
     func testCoordinatesOfNewYork() throws {
         
         // 1) 객체 생성
-        let calculator = DistancaCaculator()
+//        let calculator = DistancaCaculator()
         
         // 2) 테스트하고자하는 city 함수 호출해야함.
         // 이슈 1 : city는 Optional을 리턴함. 하지만 여기선 not nil을 기준으로 test 필요. 따라서 XCTUnwrap 이용
@@ -37,5 +35,20 @@ final class DistanceCalculatorTests: XCTestCase {
         
         XCTAssertEqual(city.coordinates.latitude, 40.7128)
         XCTAssertEqual(city.coordinates.longitude, -74.0060)
+    }
+    
+    
+    // 2. 도시 간 거리 측정하는 함수 테스트 - distance return 하는 함수
+    func testParisToNewYork() throws {
+        // 1) test를 실행할 때마다 Caculator 객체가 생성되는 것을 방지하기 위해 setUp에서 객체를 생성한다. (13, 16번째 줄)
+        
+        // 2) 두 도시 사이 거리를 계산한다.
+        let distanceInMiles = try calculator.distanceInMiles(from: "Paris", to: "NewYork")
+        
+        // 3) Equal은 아주 정확한 값을 비교해서, 원하는 테스트를 만들어내지 못함
+//        XCTAssertEqual(distanceInMiles, 3636)
+        
+        // 4) accuracy를 이용해서 해결 - accuracy는 1만큼 오차를 허용한다는 의미
+        XCTAssertEqual(distanceInMiles, 3636, accuracy: 1)
     }
 }
